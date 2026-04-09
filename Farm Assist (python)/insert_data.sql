6aeb56c3bf23eebe00dad2ebffe055757e919008
DigheSakshi2007 <sakshidighe31@gmail.com>
2026-04-09 21:26:20 +0530

    Create insert_data.sql

---
+++ Farm Assist (python)/insert_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO plants (name, category, image, disease, growth_time, description) VALUES
+('Tomato', 'vegetable', 'tomato.jpg', 'Leaf Curl', '60 days', 'Tomato plant info'),
+('Potato', 'vegetable', 'potato.jpg', 'Late Blight', '90 days', 'Potato info'),
+('Onion', 'vegetable', 'onion.jpg', 'Purple Blotch', '100 days', 'Onion info'),
+('Carrot', 'vegetable', 'carrot.jpg', 'Root Rot', '80 days', 'Carrot info'),
+('Cabbage', 'vegetable', 'cabbage.jpg', 'Black Rot', '120 days', 'Cabbage info'),
+('Apple', 'fruit', 'apple.jpg', 'Apple Scab', '120 days', 'Apple info'),
+('Mango', 'fruit', 'mango.jpg', 'Anthracnose', '150 days', 'Mango info'),
+('Banana', 'fruit', 'banana.jpg', 'Panama Disease', '300 days', 'Banana info'),
+('Orange', 'fruit', 'orange.jpg', 'Citrus Canker', '200 days', 'Orange info'),
+('Grapes', 'fruit', 'grapes.jpg', 'Downy Mildew', '180 days', 'Grapes info');
